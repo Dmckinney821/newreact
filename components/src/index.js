22e334d8369d80c273ed074b2072065399ab45ed
Dan McKinney <3dhdaniel@gmail.com>
2019-03-14 16:22:51 -0400

    Thirsty Thursday

--- components/src/index.js
+++ components/src/index.js
@@ -2,28 +2,42 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
     return (
         <div className='ui container comments'>
+        <ApprovalCard>
+            <div>
+                <h4>Warning</h4>
+                    Are you sure you want to do this?
+                
+            </div>
+        </ApprovalCard>
+        <ApprovalCard >
             <CommentDetail 
                 author='Sam' 
                 timeAgo='Today at 4:54pm' 
                 content='Yay'
                 avatar={faker.image.avatar()}
             />
+        </ApprovalCard>
+        <ApprovalCard >
             <CommentDetail 
                 author='Alex'
                 timeAgo='Today at 3:54pm' 
                 content='Huzzah'
                 avatar={faker.image.avatar()}
             />
+        </ApprovalCard>
+        <ApprovalCard>
             <CommentDetail 
                 author='Jane'
                 timeAgo='Today at 9:54am' 
                 content='boo'
                 avatar={faker.image.avatar()}
             />
+        </ApprovalCard>
          
         </div>
     );
